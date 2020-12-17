5b2f16c0fa55d8c7a69a4d0ae6d7e824cefff1ab
JorgeSerranoP <100405987@alumnos.uc3m.es>
2020-12-17 18:29:17 +0100

    Pruebas iniciales

--- CSPScheduling.py
+++ CSPScheduling.py
@@ -2,6 +2,7 @@ from constraint import *
 
 problem = Problem()
 
-problem.addVariables(['A', 'Y'])
+satelites = ['SAT1', 'SAT2', 'SAT3', 'SAT4', 'SAT5', 'SAT6']
 
-problem.addConstraint()
+problem.addVariables(satelites[1:3], ['A1F1', 'A2F1', 'A3F1', 'A4F1'])
+print(satelites[:3])
