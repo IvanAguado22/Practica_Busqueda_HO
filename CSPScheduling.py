82d773e706303e20c5a3ee9e97b6dce89f6a5dc5
JorgeSerranoP <100405987@alumnos.uc3m.es>
2020-12-19 11:12:09 +0100

    Arreglado

--- CSPScheduling.py
+++ CSPScheduling.py
@@ -3,22 +3,24 @@ from constraint import *
 problem = Problem()
 
 satelites = ['SAT1_F1', 'SAT2_F1', 'SAT3_F1',
-    'SAT3_F2' 'SAT4_F1', 'SAT5_F1', 'SAT6_F1', 'SAT6_F2']
+             'SAT3_F2', 'SAT4_F1', 'SAT5_F1', 'SAT6_F1', 'SAT6_F2']
+
 antenas = ['ANT1', 'ANT2', 'ANT3', 'ANT4', 'ANT5', 'ANT6',
            'ANT7', 'ANT8', 'ANT9', 'ANT10', 'ANT11', 'ANT12']
 
 satelitesPre12 = [satelites[0], satelites[1],
                   satelites[2], satelites[5], satelites[6]]
 
-satelitesPre12 = [satelites[3], satelites[4], satelites[7]]
+satelitesPost12 = [satelites[3], satelites[4], satelites[7]]
 
-problem.addVariables(satelites[0], antenas[:4])
-problem.addVariables(satelites[1], antenas[:3])
-problem.addVariables(satelites[2], antenas[3:6:2])
-problem.addVariables(satelites[3], (antenas[6], antenas[8], antenas[9]))
-problem.addVariables(satelites[4], (antenas[7], antenas[10], antenas[11]))
-problem.addVarible(satelites[6], antenas[6:10:2])
-problem.addVarible(satelites[7], antenas[2:5])
+problem.addVariable(satelites[0], antenas[:4])
+problem.addVariable(satelites[1], antenas[:3])
+problem.addVariable(satelites[2], antenas[3:6:2])
+problem.addVariable(satelites[3], (antenas[6], antenas[8], antenas[9]))
+problem.addVariable(satelites[4], (antenas[7], antenas[10], antenas[11]))
+problem.addVariable(satelites[5], (antenas[0], antenas[6], antenas[11]))
+problem.addVariable(satelites[6], antenas[6:10:2])
+problem.addVariable(satelites[7], antenas[2:5])
 
 
 def ConsecutiveConstraint(a, b):
@@ -27,13 +29,17 @@ def ConsecutiveConstraint(a, b):
     return True
 
 
+def TimeConstraint(arrayConstraint):
+    return False
+
+
 def SameFA7_A12(a, b):
     if(a == 'ANT7' and b == 'ANT12') or (a == 'ANT12' and b == 'ANT7'):
         return False
     return True
 
 
-problem.addConstraint(AllDifferentConstraint(), (satelites[:2])
+problem.addConstraint(AllEqualConstraint(), (satelites[:2]))
 
 problem.addConstraint(AllDifferentConstraint(),
                       (satelites[1], satelites[3], satelites[4]))
@@ -43,8 +49,11 @@ problem.addConstraint(ConsecutiveConstraint, satelites[5:3:-1])
 for i in range(len(satelitesPre12)):
     for j in range(len(satelitesPost12)):
         problem.addConstraint(
-            SameFA7_A12, (satelitesPre12[i], satelitesPost12))
+            SameFA7_A12, (satelitesPre12[i], satelitesPost12[j]))
 
-solutions=problem.getSolutions()
+solutions = problem.getSolutions()
 
-print(solutions)
+for i in solutions:
+    print(i)
+
+print(len(solutions))
